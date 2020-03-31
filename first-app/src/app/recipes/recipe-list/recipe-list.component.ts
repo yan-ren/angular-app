d285badad17b5030ae761b15cd3f02033ced820a
Yan Ren <yan.ren@indexexchange.com>
2020-03-31 00:13:48 -0400

    ngFor list

--- first-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ first-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
-    selector: 'app-recipe-list',
-    templateUrl: './recipe-list.component.html'
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent{
-    
+  recipes: Recipe[] = [new Recipe('Test', 'This is test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg')];
+
+  constructor(){}
+
+  ngOnInit(){}
 }
