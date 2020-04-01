1dfbb831b0a9c6f37077501dd72c06164c6ebf40
Yan Ren <ryan.ren3@gmail.com>
2020-03-31 23:06:34 -0400

    done with basic

--- first-app/src/app/shopping-list/shopping-list.component.ts
+++ first-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
     selector: 'app-shopping-list',
     templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent{
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 
+  constructor() { }
+
+  ogOnInit() { }
 }
