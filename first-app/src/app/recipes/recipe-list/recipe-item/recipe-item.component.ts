2501768b2de5730e9f70bacc09bb3c1b73b8ee06
Yan Ren <ryan.ren3@gmail.com>
2020-04-05 20:38:23 -0400

    property binding

--- first-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ first-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
     selector: 'app-recipe-item',
     templateUrl: './recipe-item.component.html'
 })
 export class RecipeItemComponent{
+  @Input() recipe: Recipe;
+
+  constructor(){ }
+
+  ngOnInit() {
+  }
 
 }
